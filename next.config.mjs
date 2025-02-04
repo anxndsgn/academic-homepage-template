5ef5fe60fb3859c7c1aa0e1f2efec28926165fef
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-02-04 17:26:46 +0800

    update Next.js configuration and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
-import createMDX from "@next/mdx";
+import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.bib$/,
-      use: "raw-loader",
-    });
-
-    return config;
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    turbo: {
+      rules: {
+        '*.bib': {
+          loaders: ['raw-loader'],
+          as: '*.js',
+        },
+      },
+    },
   },
 };
 
