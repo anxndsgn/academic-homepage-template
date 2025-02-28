2d9e6940e4c12801da870b8350885e569c91c684
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-02-28 19:12:30 +0800

    feat: enhance Selected Publications section with navigation link

--- components/SelectedPublicationsSection.js
+++ components/SelectedPublicationsSection.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Publications from "./Publications";
 import bibtex from "../data/bib/SelectedPublications.bib";
+import Link from "next/link";
 
 export default function SelectedPublicationsSection() {
   return (
     <section className="flex flex-col gap-3">
-      <h1 className="text-2xl font-semibold">Selected publication</h1>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold">Selected Publications</h1>
+        <Link href="/publications" className="text-neutral-500 hover:underline">
+          View all
+        </Link>
+      </div>
       <Publications bibtex={bibtex} />
     </section>
   );
